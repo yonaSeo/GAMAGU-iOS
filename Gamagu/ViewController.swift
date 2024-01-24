985395807934b9bc3083001fbd930fb6d4585826
yonaSeo <ssyon0906@gmail.com>
2024-01-25 04:07:12 +0900

    Initial Commit

---
+++ Gamagu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gamagu
+//
+//  Created by yona on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
